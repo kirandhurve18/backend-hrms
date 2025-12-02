89933a68a03c11db01e05b3f3affb5de2f64fa65
kirandhurve18 <kirand1895@gmail.com>
2025-12-02 15:40:28 +0530

    Update aws.js

--- config/aws.js
+++ config/aws.js
@@ -1,4 +1,4 @@
-const { S3Client, PutObjectCommand, GetObjectCommand, DeleteObjectCommand } = require("@aws-sdk/client-s3");
+#const { S3Client, PutObjectCommand, GetObjectCommand, DeleteObjectCommand } = require("@aws-sdk/client-s3");
 const { v4: uuidv4 } = require("uuid");
 
 // Create S3 client
